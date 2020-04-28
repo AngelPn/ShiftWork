068cb4feac3872164b3562312be5427651ae43bc
Angelina Panagopoulou <53382660+AngelPn@users.noreply.github.com>
2020-04-28 18:48:37 +0300

    Update Empl_mod.py
    Added holidays, data members SundayNightHours, HolidayHours, HolidayNightHours, maded changes in add_info to calculate holidays

--- Empl_mod.py
+++ Empl_mod.py
@@ -8,34 +8,52 @@ weekdays = {
     "Sunday" : 8
 }
 
+holidays = { # month : day
+    1 : 6,
+    3 : 25,
+    8 : 15,
+    10 : 28,
+    12 : 25
+}
+
 class Employee:
     def __init__(self, name, surname):
         self.name = name 
         self.surname = surname
         self.NightHours = 0
         self.SundayHours = 0
+        self.SundayNightHours = 0
         self.NoSickness = 0
         self.NoLicense = 0
         self.HolidayHours = 0
+        self.HolidayNightHours = 0
 
-    def add_info(self, cell, day):
-        if day == weekdays["Sunday"]:
-            if cell == "06:00 - 14:00" or cell == "14:00 - 22:00": self.SundayHours += 8
+    def add_info(self, cell, day, holiday):
+        if holiday:
+            if cell == "06:00 - 14:00" or cell == "14:00 - 22:00": self.HolidayHours += 8
+            elif cell == "22:00 - 06:00" and day == weekdays["Saturday"]:
+                self.SundayNightHours += 6
+                self.HolidayNightHours += 2
             elif cell == "22:00 - 06:00":
-                self.NightHours += 8
-                self.SundayHours += 2
+                self.NightHours += 6
+                self.HolidayNightHours += 2
             elif cell == "ΑΝΑΡ. ΑΔΕΙΑ" or cell == "ΑΣΘΕΝΕΙΑ" or cell == "ΑΔΕΙΑΣ ΚΥΗΣΗΣ" : self.NoSickness += 1
             elif cell == "ΑΔΕΙΑ": self.NoLicense += 1
             else: pass
-        elif day == weekdays["Saturday"] and cell == "22:00 - 06:00":
-            self.NightHours += 8
-            self.SundayHours +=6
         else:
-            if cell == "22:00 - 06:00": self.NightHours += 8
-            elif cell == "ΑΝΑΡ. ΑΔΕΙΑ" or cell == "ΑΣΘΕΝΕΙΑ" or cell == "ΑΔΕΙΑΣ ΚΥΗΣΗΣ": self.NoSickness += 1
-            elif cell == "ΑΔΕΙΑ": self.NoLicense += 1
-            else: pass
-
-    def print_employee(self):
-        txt = "Name: {}\nSurname: {}\nNight Hours: {}\nSunday Hours: {}\nSickness Days: {}\nLicense Days: {}\n"
-        return txt.format(self.name, self.surname, self.NightHours, self.SundayHours, self.NoSickness, self.NoLicense)
+            if day == weekdays["Sunday"]:
+                if cell == "06:00 - 14:00" or cell == "14:00 - 22:00": self.SundayHours += 8
+                elif cell == "22:00 - 06:00":
+                    self.NightHours += 6
+                    self.SundayNightHours += 2
+                elif cell == "ΑΝΑΡ. ΑΔΕΙΑ" or cell == "ΑΣΘΕΝΕΙΑ" or cell == "ΑΔΕΙΑΣ ΚΥΗΣΗΣ" : self.NoSickness += 1
+                elif cell == "ΑΔΕΙΑ": self.NoLicense += 1
+                else: pass
+            elif day == weekdays["Saturday"] and cell == "22:00 - 06:00":
+                self.NightHours += 2
+                self.SundayNightHours +=6
+            else:
+                if cell == "22:00 - 06:00": self.NightHours += 8
+                elif cell == "ΑΝΑΡ. ΑΔΕΙΑ" or cell == "ΑΣΘΕΝΕΙΑ" or cell == "ΑΔΕΙΑΣ ΚΥΗΣΗΣ": self.NoSickness += 1
+                elif cell == "ΑΔΕΙΑ": self.NoLicense += 1
+                else: pass
